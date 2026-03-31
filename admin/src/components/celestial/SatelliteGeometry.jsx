dce04eb0cbe20825bac3853b66ecc690da192825
[._.]/ Adam Eivy <atomantic@gmail.com>
2026-03-30 17:01:23 -0700

    tron sattellites

--- admin/src/components/celestial/SatelliteGeometry.jsx
+++ admin/src/components/celestial/SatelliteGeometry.jsx
@@ -2,6 +2,14 @@ import { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 
+const OUTLINE_OPACITY = 0.55
+
+const TronOutline = ({ geometry, color, scale = 1.03 }) => (
+  <lineSegments geometry={geometry} scale={scale}>
+    <lineBasicMaterial color={color} transparent opacity={OUTLINE_OPACITY} />
+  </lineSegments>
+)
+
 /**
  * Satellite-shaped composite geometry:
  * Central rectangular bus + two solar panel wings + antenna cone.
@@ -16,6 +24,10 @@ const SatelliteGeometry = ({ size, color, emissiveInt, wireframe }) => {
   const panelD = size * 0.5
   const antennaR = size * 0.08
   const antennaH = size * 0.4
+  const outlineColor = new THREE.Color('#60A5FA').lerp(new THREE.Color(color), 0.35)
+  const busEdges = new THREE.EdgesGeometry(new THREE.BoxGeometry(busW, busH, busD))
+  const panelEdges = new THREE.EdgesGeometry(new THREE.BoxGeometry(panelW, panelH, panelD))
+  const antennaEdges = new THREE.EdgesGeometry(new THREE.ConeGeometry(antennaR, antennaH, 6))
 
   const ledRef = useRef()
 
@@ -59,13 +71,14 @@ const SatelliteGeometry = ({ size, color, emissiveInt, wireframe }) => {
       <mesh>
         <boxGeometry args={[busW, busH, busD]} />
         <meshStandardMaterial
-          color="#94A3B8"
-          emissive={color}
-          emissiveIntensity={emissiveInt * 0.75}
+          color="#CBD5E1"
+          emissive={outlineColor}
+          emissiveIntensity={emissiveInt}
           roughness={0.2}
-          metalness={0.9}
+          metalness={0.92}
         />
       </mesh>
+      <TronOutline geometry={busEdges} color={outlineColor} />
       
       {/* Small status light */}
       <mesh ref={ledRef} position={[busW / 2, busH / 4, busD / 2]}>
@@ -78,36 +91,45 @@ const SatelliteGeometry = ({ size, color, emissiveInt, wireframe }) => {
         <mesh position={[-(busW / 2 + panelW / 2), 0, 0]}>
           <boxGeometry args={[panelW, panelH, panelD]} />
           <meshStandardMaterial
-            color="#1E3A8A"
-            emissive="#3B82F6"
-            emissiveIntensity={0.2}
+            color="#1D4ED8"
+            emissive="#60A5FA"
+            emissiveIntensity={0.35}
             roughness={0.1}
             metalness={0.8}
           />
         </mesh>
+        <group position={[-(busW / 2 + panelW / 2), 0, 0]}>
+          <TronOutline geometry={panelEdges} color="#60A5FA" scale={1.04} />
+        </group>
         <mesh position={[(busW / 2 + panelW / 2), 0, 0]}>
           <boxGeometry args={[panelW, panelH, panelD]} />
           <meshStandardMaterial
-            color="#1E3A8A"
-            emissive="#3B82F6"
-            emissiveIntensity={0.2}
+            color="#1D4ED8"
+            emissive="#60A5FA"
+            emissiveIntensity={0.35}
             roughness={0.1}
             metalness={0.8}
           />
         </mesh>
+        <group position={[(busW / 2 + panelW / 2), 0, 0]}>
+          <TronOutline geometry={panelEdges} color="#60A5FA" scale={1.04} />
+        </group>
       </group>
 
       {/* Antenna cone */}
       <mesh position={[0, busH / 2 + antennaH / 2, 0]}>
         <coneGeometry args={[antennaR, antennaH, 6]} />
         <meshStandardMaterial
-          color="#64748B"
-          emissive={color}
-          emissiveIntensity={emissiveInt * 0.5}
+          color="#94A3B8"
+          emissive={outlineColor}
+          emissiveIntensity={emissiveInt * 0.8}
           roughness={0.3}
           metalness={0.7}
         />
       </mesh>
+      <group position={[0, busH / 2 + antennaH / 2, 0]}>
+        <TronOutline geometry={antennaEdges} color={outlineColor} scale={1.05} />
+      </group>
     </group>
   )
 }
